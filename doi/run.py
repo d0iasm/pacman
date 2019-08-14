eb74cca2499ef34b7616106f60e83f609b6e2de7
Asami Doi <doiasami1219@gmail.com>
2019-08-14 23:47:40 +0900

    Use library in sklearn instead of custom TF-IDF.

--- doi/run.py
+++ doi/run.py
@@ -6,6 +6,8 @@ import pandas as pd
 from sklearn.naive_bayes import GaussianNB, BernoulliNB, MultinomialNB, ComplementNB
 from sklearn.model_selection import KFold
 from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 
 # Level 1
 XSS_TRAIN_FILE = 'dataset/train_level_1.csv'
@@ -13,6 +15,7 @@ XSS_TEST_FILE = 'dataset/test_level_1.csv'
 NORMAL_TRAIN_FILE = 'dataset/normal.csv'
 NORMAL_TEST_FILE = 'dataset/normal.csv'
 
+# Level 2
 XSS_TRAIN_FILE_2 = 'dataset/train_level_2.csv'
 XSS_TEST_FILE_2 = 'dataset/test_level_2.csv'
 
@@ -24,12 +27,11 @@ test_src = ""
 test_result = ""
 
 
-def data_loader(src, label):
-    data = []
-    with open(src) as f:
-        for line in f:
-            data += clean(line).split()
-    return data, [label for _ in range(len(data))]
+def data_loader(f_name, l_name):
+    with open(f_name, mode='r', encoding='utf-8') as f:
+        data = list(set(f.readlines()))
+        label = [l_name for i in range(len(data))]
+        return data, label
 
 
 def clean(t):
@@ -41,46 +43,13 @@ def clean(t):
     return t
 
 
-def vectorize(tokens):
-    total = sum([len(t) for t in tokens])
-    vec = []
-
-    # Count term frequency.
-    dicts = []
-    for t in tokens:
-        d = collections.defaultdict(int)
-        for k in t:
-            d[k] += 1
-        dicts.append(d)
-
-    # Calculate TF-IDF.
-    for i, t in enumerate(tokens):
-        for term in t:
-            if term in STOP_WORDS:
-                vec.append([term, 0])
-                continue
-
-            # TF(ti, dj) = (The count ti in dj) / (The total count ti in all documents)
-            tf = dicts[i][term] / sum([d[term] for d in dicts])
-
-            # IDF(ti) = log(The number of documents / The number of documents that has ti)
-            idf = math.log10(total / sum([1 if d[term] > 0 else 0 for d in dicts]) + 1)
-
-            if BLACK_LISTS[0] in term:
-                vec.append([term, tf * idf * 2])
-                continue
-
-            vec.append([term, tf * idf])
-    return vec
-
-
 def run():
     """
     データ作成
     """
     # Level 1
     xss_train_data, xss_train_label = data_loader(XSS_TRAIN_FILE, 'xss')
-    #xss_test_data, xss_test_label = data_loader(XSS_TEST_FILE, 'xss')
+    xss_test_data, xss_test_label = data_loader(XSS_TEST_FILE, 'xss')
     normal_train_data, normal_train_label = data_loader(NORMAL_TRAIN_FILE, 'normal')
     normal_test_data, normal_test_label = data_loader(NORMAL_TEST_FILE, 'normal')
 
@@ -91,36 +60,27 @@ def run():
     """
     データ前処理・学習機作成
     """
-    # NOTE: The argument of vectorize() must be a 2d array.
-    vec = vectorize([xss_train_data, normal_train_data, xss_train_data_2])
-    #vec = vectorize([xss_train_data_2, normal_train_data])
-
-    data = []
-
-    X_train = [n for _, n in vec]
-    X_train = np.array(X_train).reshape(-1, 1)
-
+    X_train = xss_train_data + normal_train_data + xss_train_data_2
     y_train = xss_train_label + normal_train_label + xss_train_label_2
-    #y_train = xss_train_label_2 + normal_train_label
     X_test = xss_test_data_2 + normal_test_data
     y_test = xss_test_label_2 + normal_test_label
-    #print(y_test)
+
+    vec = TfidfVectorizer()
+    X_train = vec.fit_transform(X_train)
+    X_train = X_train.todense()
 
     #model = BernoulliNB()
-    #model = GaussianNB()
-    model = MultinomialNB()
+    model = GaussianNB()
+    #model = MultinomialNB()
     #model = ComplementNB()
     model.fit(X_train, y_train)
 
     """
     テスト
     """
-    # NOTE: The argument of vectorize() must be a 2d array.
-    X_test = vectorize([xss_test_data_2, normal_test_data])
-    X_test = np.array([n for _, n in X_test], dtype=object)
-    X_test = X_test.reshape(-1, 1)
+    X_test = vec.transform(X_test)
+    X_test = X_test.todense()
     pred = model.predict(X_test)
-    #print(pred)
     acc_score = accuracy_score(y_test, pred)
     conf_mat = confusion_matrix(
         pred, y_test, labels=['xss', 'normal']
