2ba73813d6ab81dbda21f7d4c94fd6c2975fe6ac
Asami Doi <doiasami1219@gmail.com>
2019-08-15 11:08:47 +0900

    Use SVC and grid search.

--- doi/run.py
+++ doi/run.py
@@ -7,7 +7,9 @@ from sklearn.naive_bayes import GaussianNB, BernoulliNB, MultinomialNB, Compleme
 from sklearn.model_selection import KFold
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.feature_extraction.text import TfidfVectorizer
-
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
 
 # Level 1
 XSS_TRAIN_FILE = 'dataset/train_level_1.csv'
@@ -19,7 +21,11 @@ NORMAL_TEST_FILE = 'dataset/normal.csv'
 XSS_TRAIN_FILE_2 = 'dataset/train_level_2.csv'
 XSS_TEST_FILE_2 = 'dataset/test_level_2.csv'
 
-STOP_WORDS = [';']
+# Level 4
+NORMAL_TRAIN_FILE_4 = 'dataset/train_level_4.csv'
+NORMAL_TEST_FILE_4 = 'dataset/test_level_4.csv'
+
+STOP_WORDS = [';', '\"', '\'']
 BLACK_LISTS = ["alert"]
 
 keys = []
@@ -38,9 +44,15 @@ def clean(t):
     target = ["</", "/>", "<", ">", "=", ":", "/", "(", ")", "[", "]", "{", "}", "＜", "＞"]
     for ch in target:
         t = t.replace(ch, " ")
-    t = t.replace("\'", "")
-    t = t.replace("\"", "")
     return t
+
+
+def tokenize(text):
+    print(text)
+    new = []
+    for t in text():
+        new.append(t.clean())
+    return text
 
 
 def run():
@@ -57,23 +69,60 @@ def run():
     xss_train_data_2, xss_train_label_2 = data_loader(XSS_TRAIN_FILE_2, 'xss')
     xss_test_data_2, xss_test_label_2 = data_loader(XSS_TEST_FILE_2, 'xss')
 
+    # Level 4
+    normal_train_data_4, normal_train_label_4 = data_loader(NORMAL_TRAIN_FILE_4, 'normal')
+    normal_test_data_4, normal_test_label_4 = data_loader(NORMAL_TEST_FILE_4, 'normal')
+
     """
     データ前処理・学習機作成
     """
-    X_train = xss_train_data + normal_train_data + xss_train_data_2
-    y_train = xss_train_label + normal_train_label + xss_train_label_2
-    X_test = xss_test_data_2 + normal_test_data
-    y_test = xss_test_label_2 + normal_test_label
+    X_train = xss_train_data + normal_train_data + xss_train_data_2 + normal_train_data_4
+    y_train = xss_train_label + normal_train_label + xss_train_label_2 + normal_train_label_4
+    X_test = normal_test_data_4 + xss_test_data
+    y_test = normal_test_label_4 + xss_test_label
 
-    vec = TfidfVectorizer()
+    vec = TfidfVectorizer(preprocessor=clean, stop_words=STOP_WORDS)
     X_train = vec.fit_transform(X_train)
     X_train = X_train.todense()
 
+    param_grid = [
+        {
+            "kernel": ["linear"],
+            "gamma": [x/10.0 for x in range(10)],
+            "degree": [x for x in range(10)],
+            "coef0": [x/10.0 - 5.0 for x in range(10)],
+        },
+        {
+            "kernel": ["poly"],
+            "gamma": [x/10.0 for x in range(10)],
+            "degree": [x for x in range(10)],
+            "coef0": [x/10.0 - 5.0 for x in range(10)],
+        },
+        {
+            "kernel": ["rbf"],
+            "gamma": [x/10.0 for x in range(10)],
+            "degree": [x for x in range(10)],
+            "coef0": [x/10.0 - 5.0 for x in range(10)],
+        },
+        {
+            "kernel": ["sigmoid"],
+            "gamma": [x/10.0 for x in range(10)],
+            "degree": [x for x in range(10)],
+            "coef0": [x/10.0 - 5.0 for x in range(10)],
+        },
+    ]
+
+    model = GridSearchCV(estimator=SVC(), param_grid=param_grid, scoring='accuracy', cv=5)
+    model.fit(X_train, y_train)
+    print('=============')
+    print(model.best_params_)
+    print('=============')
+
     #model = BernoulliNB()
-    model = GaussianNB()
+    #model = GaussianNB()
     #model = MultinomialNB()
     #model = ComplementNB()
-    model.fit(X_train, y_train)
+    #model.fit(X_train, y_train)
 
     """
     テスト
@@ -85,6 +134,15 @@ def run():
     conf_mat = confusion_matrix(
         pred, y_test, labels=['xss', 'normal']
     )
+
+    idx = 0
+    for p, a in zip(pred, y_test):
+        if p != a:
+          print("-----------")
+          print("ans: ", a, " pred:", p)
+          print((normal_test_data_4 + xss_test_data)[idx])
+        idx += 1
+
     print("acc: \n", acc_score)
     print("confusion matrix: \n", conf_mat)
 
